c3db1f9eec58b1f267421c94fa344d25b126af50
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2021-08-01 10:41:57 -0300

    Update job.sh

--- rundeck/3-job-parametrizado/job.sh
+++ rundeck/3-job-parametrizado/job.sh
@@ -6,7 +6,8 @@ su -c 'mkdir -p ~/.ssh' @option.user@
 # É verificado se a chave pública já está presente dentro do arquivo "authorized_keys" no diretório ".ssh" na home do usuário e jogamos as saídas (stdout e stderr) para o limbo dentro do linux.
 grep "@option.key@" /home/@option.user@/.ssh/authorized_keys > /dev/null
 
-# Aqui temos uma condição que irá validar o return code do comando anterior (grep), caso o return code seja diferente de 0, ou seja, retornou com erro, a chave será concatenada ao final do arquivo authorized_keys. Caso retorne 0, que siginifica que rodou com sucesso, simplesmente será apresentada a mensagem "Chave já existe".
+# Aqui temos uma condição que irá validar o return code do comando anterior (grep), caso o return code seja diferente de 0, ou seja, retornou com erro, a chave será concatenada ao final do arquivo authorized_keys. Caso ret
+orne 0, que siginifica que rodou com sucesso, simplesmente será apresentada a mensagem "Chave já existe".
 
 if [ $? !=0 ]; then
     su -c 'echo "@option.key@" >> ~/.ssh/authorized_keys' @option.user@
@@ -16,6 +17,4 @@ else
 fi
 
 # Por fim, é criado a autorização para que este novo usuário possa utilizar o SUDO.
-
 echo '@option.user@ ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/@option.user@
-
